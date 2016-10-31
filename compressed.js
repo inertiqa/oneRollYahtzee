8e8624755f27c028cf4953323a4d3059f12d6afb
Ekenmer <ekenmer@gmail.com>
2016-10-31 17:57:10 +0100

    Update compressed.js

--- compressed.js
+++ compressed.js
@@ -2,5 +2,5 @@ var diceToss = [0,0]; // Counter
 let random = () => Math.floor(Math.random() * 1296 + 1); // 1 in 1296 random generator
 do { diceToss[0] = random(); // YHTZ Game Engine 1
 	diceToss[1]++; // YHTZ Game Engine 2
-} while (diceToss[0] != 666); // YHTZ Game Engine 3 control number
+} while (diceToss[0] != 666); // YHTZ Game Engine 3, control number
 console.log = "Yahtzee took " + diceToss[1] + " throws!"; // Displays stats at true
