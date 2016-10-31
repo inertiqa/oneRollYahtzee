e302cf77980282d21ad2dd3bf11d53dfe8af6724
Ekenmer <ekenmer@gmail.com>
2016-10-31 12:23:58 +0100

    Update compressed.js

--- compressed.js
+++ compressed.js
@@ -1,18 +1,7 @@
-// Counter
-var diceToss = [0,0];
-
-// 1 in 1296 random generator
-function random() {
-    a = Math.floor(Math.random() * 1296 + 1);
-    return a;
-}
-
-// Gets a random number, compares it to the control number, and adds +1 to the throw counter
-do {
+var diceToss = [0,0]; // Counter
+let random = () => Math.floor(Math.random() * 1296 + 1); // 1 in 1296 random generator
+do { // YHTZ Game Engine
 	diceToss[0] = random();
 	diceToss[1]++;
-}
-while (diceToss[0] != 666);
-
-// Displays stats when you get a positive
-console.log = "Yahtzee took "  + diceToss[1] + " throws!";
+} while (diceToss[0] != 666); // Control number
+console.log = "Yahtzee took " + diceToss[1] + " throws!"; // Displays stats at true
