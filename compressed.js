8584164ba97ff62153a503ce1a3a62ca3690a409
Ekenmer <ekenmer@gmail.com>
2016-11-03 13:00:44 +0100

    Update compressed.js

--- compressed.js
+++ compressed.js
@@ -1,4 +1,4 @@
-var diceToss = [0,0]; // Counter
+var diceToss = [0,0]; // Counter // Working Demo: https://repl.it/ENfh
 let random = () => Math.floor(Math.random() * 1296 + 1); // 1 in 1296 random generator
 do { diceToss[0] = random(); // YHTZ Game Engine 1
 	diceToss[1]++; // YHTZ Game Engine 2
