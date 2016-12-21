6c21d5bab0ebc6d031d45565d1adc4f7ae170951
Ekenmer <ekenmer@gmail.com>
2016-12-21 02:25:34 +0100

    Update yahtzeeCompressed.lua

--- yahtzeeCompressed.lua
+++ yahtzeeCompressed.lua
@@ -1,8 +1,6 @@
-a = 0
-b = 0
--- second iteration
-while (a ~= 666) do
-  a = math.random (1,1296)
-  b = b + 1
-  print(a, b)
-end
+array = {0, 0}
+while (array[1] ~= 666) do
+  array[1] = math.random (1,1296)
+  array[2] = array[2] + 1
+  print(array[1], array[2])
+end -- array first iteration
