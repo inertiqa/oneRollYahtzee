b73b267c1bcaa11108a87a0767cb8652307c36f5
Ekenmer <ekenmer@gmail.com>
2016-12-21 02:02:06 +0100

    Update compressed.js

--- compressed.js
+++ compressed.js
@@ -1,6 +1,6 @@
 var diceToss = [0,0]; // Counter // Working Demo: https://repl.it/ENfh
-let random = () => Math.floor(Math.random() * 1296 + 1); // 1 in 1296 random generator "magic number"
-do { diceToss[0] = random(); // YHTZ Game Engine 1
-	diceToss[1]++; // YHTZ Game Engine 2
-} while (diceToss[0] != 666); // YHTZ Game Engine 3, lucky number
+let random = () => Math.floor(Math.random() * 1296 + 1); // 1 in 1296 random generator
+do { diceToss[0] = random(); // Random number stored in array index 0 						-Loop Starts
+	diceToss[1]++; // Increases dice counter by 1
+} while (diceToss[0] != 666); // We are playing with the number 666, checks if we get lucky this time		-Loop Ends
 console.log = "Yahtzee took " + diceToss[1] + " throws!"; // Displays stats at true
