db59d43c5387854dee29177fe2e221a0a40621c6
OOQQ <ooqq@yahoo.com>
2017-07-12 00:19:58 +0200

    Update yahtzeeCompressed.lua

--- yahtzeeCompressed.lua
+++ yahtzeeCompressed.lua
@@ -1,6 +1,6 @@
-array = {0, 0} --Random number & counter. Working Demo: https://repl.it/Euo8
+array = {0, math.random (1,1296)} --Random number & counter. Working Demo: https://repl.it/Euo8
 while (array[2] ~= 666) do --We are playing with the number 666, checks if we get lucky this time		Loop Starts
-  array[2] = math.random (1,1296) --1 in 1296 random generator
   array[1] = array[1] + 1 --Increases dice counter by 1
+  array[2] = math.random (1,1296) --1 in 1296 random generator
+  print(array[1], array[2]) --Display stats at true
 end --Loop Ends
-print(("Yathzee in "), array[1]) --Display stats at true
