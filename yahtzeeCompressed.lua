b00d5a5348d3f58a05ec5278b50ed54989a0dfb4
OOQQ <ooqq@yahoo.com>
2017-07-12 07:29:45 +0200

    Update yahtzeeCompressed.lua

--- yahtzeeCompressed.lua
+++ yahtzeeCompressed.lua
@@ -1,4 +1,4 @@
-array = {0, math.random (1,1296)} --Random number & counter. Working Demo: https://repl.it/Euo8
+array = {0, math.random (1,1296)} --Random number & counter. By OOQQ
 while (array[2] ~= 666) do --We are playing with the number 666, checks if we get lucky this time		Loop Starts
   array[1] = array[1] + 1 --Increases dice counter by 1
   array[2] = math.random (1,1296) --1 in 1296 random generator
