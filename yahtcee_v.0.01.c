f198b69617321fe6575dcb85be0d7b4dac38f4d9
OOQQ <ooqq@yahoo.com>
2020-07-24 14:33:34 +0100

    Update and rename yahtcee.c to yahtcee_v.0.01.c

--- yahtcee_v.0.01.c
+++ yahtcee_v.0.01.c
@@ -1,4 +1,10 @@
 //by OOQQ
+//dices [dice 1 throw], [number of 1's]
+//      [dice 2 throw], [number of 2's]
+//      [dice 3 throw], [number of 3's]
+//      [dice 4 throw], [number of 4's]
+//      [dice 5 throw], [number of 5's]
+//      [trow counter], [number of 6's]
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -10,29 +16,29 @@ int main(void) {
 	srand((unsigned) time(NULL));
 
 	do {
-		for(i = 0; i < 5; i++) { //resets the results dimension
+		for(i = 0; i < 5; i++) { //resets the results array
 			dices[1][i] = 0;
 		}
 
-		for(i = 0; i < 5; i++) { //populates the throw dimension
+		for(i = 0; i < 5; i++) { //populates the throw array
 			dices[0][i] = (rand() % 6) + 1;
 		}
 
-		dices[0][5] += 1; //ticks the throw counter +1
+		dices[0][5]++; //ticks the throw counter +1
 
-		for(i = 0; i < 5; i++) { //populates the results dimension
+		for(i = 0; i < 5; i++) { //populates the results array
 			switch(dices[0][i]) {
-				case 1: dices[1][0] += 1;
+				case 1: dices[1][0]++;
 					break;
-				case 2: dices[1][1] += 1;
+				case 2: dices[1][1]++;
 					break;
-				case 3: dices[1][2] += 1;
+				case 3: dices[1][2]++;
 					break;
-				case 4: dices[1][3] += 1;
+				case 4: dices[1][3]++;
 					break;
-				case 5: dices[1][4] += 1;
+				case 5: dices[1][4]++;
 					break;
-				default : dices[1][5] += 1;
+				default : dices[1][5]++;
 					break;
 			}
 		}
@@ -53,10 +59,10 @@ int main(void) {
 		}
 
 	} while(dices[1][0] < 5 && //loop while not yathCee
- 			 dices[1][1] < 5 &&
-			 dices[1][2] < 5 &&
-			 dices[1][3] < 5 &&
-			 dices[1][4] < 5);
+		dices[1][1] < 5 &&
+		dices[1][2] < 5 &&
+		dices[1][3] < 5 &&
+		dices[1][4] < 5);
 
 return 0;
 }
